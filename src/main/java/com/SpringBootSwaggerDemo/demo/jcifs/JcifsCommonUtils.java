24f6a85fbc1d0650d17429edc2c65c6103a99a37
chenggong zhou <26921832@qq.com>
2018-11-29 16:38:53 +0800

    新增公共类，调用linux，ssh ，调用window共享文件盘，解析ini文件

---
+++ src/main/java/com/SpringBootSwaggerDemo/demo/jcifs/JcifsCommonUtils.java
@@ -0,0 +1,123 @@
+package com.SpringBootSwaggerDemo.demo.jcifs;
+
+import jcifs.smb.NtlmPasswordAuthentication;
+import jcifs.smb.SmbException;
+import jcifs.smb.SmbFile;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author zhoucg
+ * @date 2018-11-29
+ * 使用CIFS SMB 第三方公共包操作window系统共享文件盘数据
+ * <p>
+ *     CIFS是公共的或开放的SMB协议版本，并由Microsoft使用。SMB协议现在是局域网上用于服务器文件访问和打印的协议。
+ *     象SMB协议一样，CIFS在高层运行，而不象TCP/IP协议那样运行在底层。CIFS可以看做是应用程序协议如文件传输协议和超文本传输协议的一个实现。
+ * </p>
+ */
+public class JcifsCommonUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(JcifsCommonUtils.class);
+
+    /**
+     * 协议名称
+     */
+    private static final String PROTOCOL_PRE = "smb://";
+
+    /**
+     * 解析window系统共享文件盘指定盘符，获取指定文件结果数据
+     * @param remoteHost
+     * @param remoteUserName
+     * @param remotePassword
+     * @param remteDir
+     * @return
+     */
+    public static Map<String,Object> deepParseSmbDir(String remoteHost, String remoteUserName,
+                                                     String remotePassword, String remteDir) throws Exception{
+
+        logger.info("ip地址：{},用户名：{}，密码：{}，文件路径：{}",remoteHost,remoteUserName,remotePassword,remteDir);
+
+        Map<String,Object> smbMap = new HashMap<>(16);
+        smbMap.put("hostIp",remoteHost);
+        smbMap.put("diskUseRate","62%");
+
+        NtlmPasswordAuthentication auth = new NtlmPasswordAuthentication(PROTOCOL_PRE + remoteHost,remoteUserName,remotePassword );
+        logger.info("当前获取auth:{}",auth);
+
+        /**
+         * 共享文件夹远程url
+         */
+        String remoteUrl = PROTOCOL_PRE + remoteUserName + ":" + remotePassword + "@" + remoteHost + "/" + remteDir + "/";
+        logger.info("remoteUrl:{}",remoteUrl);
+
+        try{
+            SmbFile file = new SmbFile(remoteUrl,auth);
+            if(file.isDirectory()) {
+                List<Map<String,String>> fileList = new ArrayList<>();
+                SmbFile[] files = file.listFiles();
+                /**
+                 * behz
+                 * SL0
+                 * SL1
+                 */
+                for (SmbFile smbFile : files) {
+                    if(smbFile.isDirectory()) {
+
+                        if(smbFile.getName().contains("behz")) {
+                            continue;
+                        }
+                        /**
+                         * 统计当前<p>remteDir</p>下为文件夹的资料进行统计
+                         */
+                        Map<String,String> smbDirMap = new HashMap<>(16);
+                        String chkDsk = smbFile.getName();
+                        if(chkDsk.indexOf("/") != -1) {
+                            int lastIndexOf = chkDsk.lastIndexOf("/");
+                            chkDsk = chkDsk.substring(0,lastIndexOf);
+                        }
+                        smbDirMap.put("chkdsk",chkDsk);
+                        int total = recursionFile(smbFile);
+                        Long length = smbFile.length();
+                        logger.info("current scan sourceName:{},scan total:{},scan length:{}",smbFile.getName(),total,length);
+                        smbDirMap.put("total",String.valueOf(total));
+                        fileList.add(smbDirMap);
+                    }
+                }
+                smbMap.put("diskList",fileList);
+
+            }
+
+        } catch (MalformedURLException e) {
+            logger.error("current scan the filepath:{} error ,current error message:{}",remoteUrl,e.getMessage());
+            throw e;
+        }
+
+        return smbMap;
+
+    }
+
+    /**
+     * 递归的方式获取指定文件目录下的目录信息
+     * @param smbFile
+     * @return
+     */
+    private static int recursionFile(SmbFile smbFile) throws MalformedURLException,SmbException {
+        int i=0;
+        SmbFile[] dirFiles = smbFile.listFiles();
+        for (SmbFile dirFile : dirFiles) {
+            if(!dirFile.isDirectory()) {
+                i++;
+            } else {
+                i+= recursionFile(dirFile);
+            }
+        }
+        return i;
+    }
+}
---
+++ src/main/java/com/SpringBootSwaggerDemo/demo/jsch/SftpUtils.java
@@ -0,0 +1,244 @@
+package com.SpringBootSwaggerDemo.demo.jsch;
+
+import com.jcraft.jsch.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.*;
+
+/**
+ *
+ * @author zhoucg
+ * @date 2018-11-29
+ * 远程linux系统，将linux系统目录下的指定文件夹，
+ * 调用命令
+ */
+public class SftpUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(SftpUtils.class);
+
+    /**
+     * 新增线程池操作获取sftp连接
+     */
+    private static final Map<String,Channel> SFTP_CHANNEL_POOL = new HashMap<>();
+
+    /**
+     * 默认编码格式
+     */
+    private static final String CHARSET_UTF8 = "UTF-8";
+
+
+    /**
+     * 多线程下获取ftp连接
+     * @param host
+     * @param port
+     * @param userName
+     * @param password
+     * @param threadId
+     * @return
+     */
+    public static ChannelSftp getSftpConnection(final String host, final int port, final String userName,
+                                                final String password, final long threadId) throws JSchException {
+        Channel channel = null;
+        ChannelSftp sftp = null;
+        String key = host + "," + port + "," + userName + "," +password + "," +threadId;
+        channel = doConnection(host,port,userName,password,key);
+        sftp = (ChannelSftp) channel;
+        return sftp;
+    }
+
+    /**
+     * 单线程下获取连接
+     * @param host
+     * @param port
+     * @param userName
+     * @param password
+     * @return
+     */
+    public static ChannelSftp getSftpConnection(final String host,final int port,final String userName,
+                                                final String password) throws JSchException {
+        Channel channel = null;
+        ChannelSftp sftp = null;
+        String key = host + "," + port + "," + userName + "," +password;
+        channel = doConnection(host,port,userName,password,key);
+        sftp = (ChannelSftp) channel;
+        return sftp;
+    }
+
+
+    /**
+     * 使用Map存储对应的sftp连接，连接池
+     * @param host
+     * @param port
+     * @param userName
+     * @param password
+     * @param key
+     * @return
+     * @throws JSchException
+     */
+    public static Channel doConnection (String host,int port,String userName,String password,String key) throws JSchException {
+        Session sshSession = null;
+        Channel channel = null;
+        ChannelSftp sftp = null;
+//        if(null == SFTP_CHANNEL_POOL.get(key)) {
+        JSch jsch = new JSch();
+        jsch.getSession(userName, host, port);
+        sshSession = jsch.getSession(userName, host, port);
+        sshSession.setPassword(password);
+        Properties sshConfig = new Properties();
+        sshConfig.put("StrictHostKeyChecking", "no");
+        sshSession.setConfig(sshConfig);
+        sshSession.connect();
+        channel = sshSession.openChannel("sftp");
+        channel.connect();
+//            SFTP_CHANNEL_POOL.put(key, channel);
+//        } else {
+//            channel = SFTP_CHANNEL_POOL.get(key);
+//            sshSession = channel.getSession();
+//            if (!sshSession.isConnected()) {
+//                sshSession.connect();
+//            }
+//            if (!channel.isConnected()) {
+//                channel.connect();
+//            }
+//        }
+        return channel;
+    }
+
+
+    /**
+     * 关闭协议-sftp协议.(关闭会导致连接池异常，因此不建议用户自定义关闭)
+     * @param sftp sftp连接
+     */
+    public static void exit(final ChannelSftp sftp ) {
+        if(sftp != null) {
+            sftp.exit();
+        }
+    }
+
+
+    /**
+     * 调用ssh命令，返回输出命令的结果信息
+     * @param cmd
+     * @param charset
+     * @param sft
+     * @return BufferedReader 返回输出结果的字符串
+     * @throws Exception
+     */
+    public static BufferedReader runCmd(String cmd, String charset, final ChannelSftp sft) throws Exception{
+        Session session;
+        ChannelExec channelExec;
+        try{
+            session = sft.getSession();
+            channelExec = (ChannelExec) session.openChannel("exec");
+
+            channelExec.setCommand(cmd);
+            channelExec.setInputStream(null);
+            channelExec.setErrStream(System.err);
+            channelExec.connect();
+            InputStream in = channelExec.getInputStream();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            return reader;
+        } catch (Exception e) {
+            e.getMessage();
+            throw e;
+        }
+    }
+
+    /**
+     * 将远程的文件转换成io流的方式返回
+     * @param filePath
+     * @param sftp
+     * @return
+     */
+    public static InputStream readFile(final String filePath,final ChannelSftp sftp) {
+        InputStream in = null;
+        try{
+            in = sftp.get(filePath);
+        } catch (Exception e ){
+            logger.error("当前读取文件:{}错误，错误堆栈信息:{}",filePath,e.getMessage());
+        }
+        return in;
+    }
+
+    /**
+     * 将远程的文件夹下的目录，获取最新的创建时间的 newLength 个数据
+     * ls -lt /dirname/ | grep filename | head -n 1 |awk '{print $9}'
+     * /dirname/：指定的文件夹目录
+     * grep filename :匹配的文件名信息
+     * 1:表示显示最新的几个数据信息
+     * @param fileDir
+     * @param sftp
+     * @param least
+     * @return
+     *          list:
+     *              key:resourceName -> value:notice message
+     */
+    public static List<Map<String,String>> readDir(final String fileDir, final ChannelSftp sftp, int least) {
+        List<Map<String,String>> newMapList = new ArrayList<>(16);
+        String searchCmd = "ls -lt " + fileDir + " | grep notice | head -n " + least;
+        logger.info("curret query readDir cmd = "+searchCmd);
+        BufferedReader bufferedReader;
+        try{
+            bufferedReader = runCmd(searchCmd,CHARSET_UTF8,sftp);
+            String line;
+            while((line = bufferedReader.readLine()) != null) {
+                logger.info("current line：{}",line);
+                Map<String,String> newMap = new HashMap<>(4);
+                String[] lineArr = line.split(" ");
+                String linePath = lineArr[lineArr.length-1];
+                String fullPath = fileDir + linePath;
+                StringBuilder stringBuilder = new StringBuilder();
+                InputStream in = readFile(fullPath,sftp);
+                BufferedReader localReader = new BufferedReader(new InputStreamReader(in));
+                int i = 0;
+                String localLine;
+                while((localLine = localReader.readLine()) != null) {
+                    i++;
+                    if(i == 6) {
+                        /**
+                         * 主题信息
+                         */
+                        newMap.put("noticeTitle",localLine);
+                    }
+                    if( i > 6) {
+                        stringBuilder.append(localLine);
+                    }
+                }
+                newMap.put(fullPath,stringBuilder.toString());
+                newMapList.add(newMap);
+                in.close();
+                localReader.close();
+            }
+        } catch ( Exception e) {
+            logger.error("SftpUtil query readDir error , fileDir:{},cmd:{},error message{}",fileDir,searchCmd,e);
+            return newMapList;
+        }
+        close(bufferedReader);
+        return newMapList;
+    }
+
+    /**
+     * 关闭流
+     * @param reader
+     */
+    private static void close(Reader reader) {
+        try{
+            if( reader != null) {
+                reader.close();
+            }
+        } catch ( Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
+
+
+}
