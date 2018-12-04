5df6eaf66010b66b0f2d2412c1a42593e34ee779
chenggong zhou <26921832@qq.com>
2018-12-04 15:14:19 +0800

    线程使用

---
+++ src/main/java/com/common/util/thread/JoinThread.java
@@ -0,0 +1,72 @@
+package com.common.util.thread;
+
+/**
+ * Created by zhoucg on 2018-11-22.
+ */
+public class JoinThread {
+
+
+    /**
+     * 程序设计，t1,t2,t3三个线程，保持三个线程同时执行，并且让t1,t2执行完之后t3才执行
+     *
+     * join()方法：
+     *          当某个程序执行流中调用其他线程的join方法，调用线程被阻塞，直到新join的线程执行完。
+     *          假设现在有两个线程A、B。如果在A的run方法中调用B.join()，表示A需要在B线程上面等待，也就是需要在B线程执行完成之后才能再次执行
+     * @param args
+     */
+    public static void main(String[] args) throws Exception{
+        /**
+         * 线程t1
+         */
+        Thread t1 = new Thread(() -> {
+            try{
+                System.out.println("Thread t1 is running...");
+                Thread.sleep(1000);
+            } catch ( Exception e ) {
+
+            }
+        });
+
+        /**
+         * 线程t2
+         */
+        Thread t2 = new Thread(() ->{
+           try{
+               System.out.println("Thread t2 is running...");
+               Thread.sleep(1000);
+           } catch ( Exception e ){
+
+           }
+        });
+
+        /**
+         * 线程t3
+         */
+        Thread t3 = new Thread(() ->{
+           try{
+               System.out.println("Thread t3 is running...");
+               Thread.sleep(1000);
+           } catch ( Exception e) {
+
+           }
+        });
+
+//        t1.start();
+//        t1.join(); //只会使主线程进入等待状态并等待t1线程执行完毕之后才被唤醒
+//
+//        t2.start();
+//        t2.join();
+//
+//        t3.start();
+
+
+        t1.start();
+        t1.join();
+
+        t2.start();
+
+        Thread.sleep(1000);
+        System.out.println("thread main is running ...");
+
+}
+}
---
+++ src/main/java/com/common/util/thread/ThreadPools.java
@@ -0,0 +1,35 @@
+package com.common.util.thread;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
+
+import java.util.concurrent.*;
+
+/**
+ *
+ * @author zhoucg
+ * @date 2018-11-25
+ * alibaba 线程池的使用规范
+ */
+public class ThreadPools  {
+
+    public static void main(String[] args) {
+
+        /**
+         * java线程池使用的正确方式
+         *
+         * 注意：最近阿里发布的 Java开发手册中强制线程池不允许使用 Executors 去创建，
+         * 而是通过 ThreadPoolExecutor 的方式，这样的处理方式让写的同学更加明确线程池的运行规则，
+         * 规避资源耗尽的风险。下面转载部分有较为详细的介绍
+         */
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
+                .setNameFormat("demo-pool-%d").build();
+
+        ExecutorService executorService = new ThreadPoolExecutor(5,10,0L,
+                TimeUnit.MILLISECONDS,new LinkedBlockingQueue<>(1024), namedThreadFactory,new ThreadPoolExecutor.AbortPolicy());
+
+
+        executorService.execute(() ->{
+            System.out.println(Thread.currentThread().getName());
+        });
+    }
+}
