b800fdf7119b8f4ed4716720102ee1f3d344417c
chenggong zhou <26921832@qq.com>
2019-03-17 11:02:42 +0800

    jdk动态代理和cglib动态代理

---
+++ src/main/java/com/common/util/proxy/HelloInteceptor.java
@@ -0,0 +1,21 @@
+package com.common.util.proxy;
+
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+/**
+ * Created by zhoucg on 2019-03-17.
+ * CGLIB代理不需要目标类实现接口，
+ * 利用cglib动态代理生成字节码文件
+ */
+public class HelloInteceptor implements MethodInterceptor {
+    @Override
+    public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
+        System.out.println(">>>>>>开启事务<<<<<<");
+        Object proxyObject = methodProxy.invokeSuper(o, objects);
+        System.out.println(">>>>>>关闭事务<<<<<<");
+        return proxyObject;
+    }
+}
---
+++ src/main/java/com/common/util/proxy/ProxyTest.java
@@ -0,0 +1,28 @@
+package com.common.util.proxy;
+
+import java.lang.reflect.Proxy;
+
+/**
+ * Created by zhoucg on 2019-03-17.
+ */
+public class ProxyTest {
+
+    public static void main(String[] args) {
+        //设置为true,会在工程根目录生成$Proxy0.class代理类（com.sun.proxy.$Proxy0.class）
+        System.getProperties().put(
+                "sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+
+        String saveGeneratedFiles = System.getProperty("sun.misc.ProxyGenerator.saveGeneratedFiles");
+        System.out.println(saveGeneratedFiles);
+
+        HelloTest helloWord = new HelloTestImpl();
+        CustomInvocationHandler customInvocationHandler = new CustomInvocationHandler(
+                helloWord);
+        //通过Proxy.newProxyInstance生成代理对象
+        HelloTest proxy = (HelloTest) Proxy.newProxyInstance(
+                HelloTest.class.getClassLoader(),
+                helloWord.getClass().getInterfaces(), customInvocationHandler);
+        //调用say方法
+        proxy.say("test");
+    }
+}
