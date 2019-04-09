e7561e66294e0a1f849c2d27ba9322ca7222bad5
chenggong zhou <26921832@qq.com>
2019-04-09 16:29:12 +0800

    修改指定的spake的注释，线程示例新增

--- src/main/java/com/common/util/Test.java
+++ src/main/java/com/common/util/Test.java
@@ -2,10 +2,13 @@ package com.common.util;
 
 
 import org.apache.commons.lang3.StringUtils;
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
 
+import java.text.SimpleDateFormat;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -91,19 +94,90 @@ public class Test {
 //            System.out.println(strA);
 //        }
 
-        String test = "-100";
-        int testInt = Integer.parseInt(test);
-        int sbtestInt = -testInt;
-        System.out.println(sbtestInt);
-        System.out.println(testInt);
-        test = test.substring(1);
-        boolean flag = StringUtils.isNumeric(test);
-        System.out.println(flag);
+//        String test = "-100";
+//        int testInt = Integer.parseInt(test);
+//        int sbtestInt = -testInt;
+//        System.out.println(sbtestInt);
+//        System.out.println(testInt);
+//        test = test.substring(1);
+//        boolean flag = StringUtils.isNumeric(test);
+//        System.out.println(flag);
+//
+//        Date date = new Date();
+//        ZonedDateTime d = ZonedDateTime.ofInstant(date.toInstant(),ZoneId.of("GMT-0"));
+//        System.out.println(d);
 
-        Date date = new Date();
-        ZonedDateTime d = ZonedDateTime.ofInstant(date.toInstant(),ZoneId.of("GMT-0"));
-        System.out.println(d);
+//        String str = timeStamp2Date("1553483333043","yyyy-MM-dd HH:mm:ss.SSS");
+//        System.out.println(str);
 
+//        int a =18;
+//        int b = a >>> 1;
+//        System.out.println(b);
+
+//        ConcurrentHashMap<String,String> concurrentHashMap = new ConcurrentHashMap<>(18);
+//        concurrentHashMap.put("a","b");
+
+        List<User> userList = new ArrayList<>();
+        userList.add(new User("1","A.0001.0003.R001:A.0013.0001.P006:A.0013.0001.S001"));
+        userList.add(new User("1","J.0003.0003.R002:J.0003.0003.P002:J.0003.0003.S001"));
+        userList.add(new User("1","M.0032.0002.R001:M.0033.0001.P002:M.0033.0001.S001"));
+        userList.add(new User("1","J.0003.0014.R001:J.0003.0014.P001:J.0003.0014.S001"));
+        userList.add(new User("1","A.0001.0040.R001:A.0001.0032.PA02:A.0001.0025.S004"));
+        userList.add(new User("1","A.0001.0032.R001:A.0010.0001.P006:A.0010.0001.S001"));
+        userList.add(new User("1","A.0001.0031.R001:A.0011.0001.P001:A.0011.0001.S001"));
+        userList.add(new User("1","B.0001.0008.R001:B.0001.0008.PT01:B.0001.0024.S001"));
+        userList.add(new User("1","G.0002.0004.R001:G.0002.0004.P001:G.0002.0001.S001"));
+
+        System.out.println(userList);
+
+
+
+    }
+
+    private static class User{
+        private String name;
+        private String code;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getCode() {
+            return code;
+        }
+
+        public void setCode(String code) {
+            this.code = code;
+        }
+
+        public User(String name,String code) {
+            this.name = name;
+            this.code = code;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "name='" + name + '\'' +
+                    ", code='" + code + '\'' +
+                    '}';
+        }
+    }
+
+
+    public static String timeStamp2Date(String seconds,String format) {
+                 if(seconds == null || seconds.isEmpty() || seconds.equals("null")){
+                         return "";
+                     }
+                 if(format == null || format.isEmpty()){
+                         format = "yyyy-MM-dd HH:mm:ss";
+                     }
+                 SimpleDateFormat sdf = new SimpleDateFormat(format);
+                 return sdf.format(new Date(Long.valueOf(seconds+"000")));
     }
 
     public static Date getYesterdayDay() {
--- src/main/java/com/common/util/spark/PeopleInfoCalculator.java
+++ src/main/java/com/common/util/spark/PeopleInfoCalculator.java
@@ -1,8 +1,8 @@
 package com.common.util.spark;
 
-import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaRDD;
-import org.apache.spark.api.java.JavaSparkContext;
+//import org.apache.spark.SparkConf;
+//import org.apache.spark.api.java.JavaRDD;
+//import org.apache.spark.api.java.JavaSparkContext;
 
 import java.util.Arrays;
 
@@ -37,37 +37,37 @@ public class PeopleInfoCalculator {
      */
     public static void main(String[] args) {
 
-        SparkConf sparkConf = new SparkConf().setAppName("PeopleInfoCalculator").setMaster("local[2]");
-
-        JavaSparkContext sc = new JavaSparkContext(sparkConf);
-        JavaRDD<String> dataFile = sc.textFile("F:\\PeopleInfo.txt");
-
-
-        JavaRDD<String> maleFilterData = dataFile.filter(s -> s.contains("M"));
-        JavaRDD<String> femaleFilterData = dataFile.filter(s -> s.contains("F"));
-        JavaRDD<String> maleHeightData = maleFilterData.flatMap(s -> Arrays.asList(s.split(" ")[2]).iterator());
-        JavaRDD<String> femaleHeightData = femaleFilterData.flatMap(s -> Arrays.asList(s.split(" ")[2]).iterator());
-        JavaRDD<Integer> maleHeightDataInt = maleHeightData.map(s -> Integer.parseInt(String.valueOf(s)));
-        JavaRDD<Integer> femaleHeightDataInt = femaleHeightData.map(s -> Integer.parseInt(String.valueOf(s)));
-        //sortBy(<T>,ascending,numPartitions) 解释:
-        //第一个参数是一个函数，该函数的也有一个带T泛型的参数，返回类型和RDD中元素的类型是一致的；
-        //第二个参数是ascending，这参数决定排序后RDD中的元素是升序还是降序，默认是true，也就是升序；
-        //第三个参数是numPartitions，该参数决定排序后的RDD的分区个数，默认排序后的分区个数和排序之前的个数相等，即为this.partitions.size。
-        JavaRDD<Integer> maleHeightLowSort = maleHeightDataInt.sortBy(s -> s,true,3);
-        JavaRDD<Integer> femaleHeightLowSort = femaleHeightDataInt.sortBy(s -> s,true,3);
-        JavaRDD<Integer> maleHeightHightSort = maleHeightDataInt.sortBy(s -> s,false,3);
-        JavaRDD<Integer> femaleHeightHightSort = femaleHeightDataInt.sortBy(s -> s,false,3);
-        Integer lowestMale = maleHeightLowSort.first(); //求出升序的第一个数，即最小值
-        Integer lowestFemale = femaleHeightLowSort.first();//求出升序的第一个数，即最小值
-        Integer highestMale = maleHeightHightSort.first();//求出降序的第一个数，即最大值
-        Integer highestFemale = femaleHeightHightSort.first();//求出降序的第一个数，即最大值
-
-        System.out.println("Number of Female Peole:" + femaleHeightData.count());//求出女性的总个数
-        System.out.println("Number of Male Peole:" + maleHeightData.count());//求出男性的总个数
-        System.out.println("Lowest Male:" + lowestMale);//求出男性最矮身高
-        System.out.println("Lowest Female:" + lowestFemale);//求出女性最矮身高
-        System.out.println("Highest Male:" + highestMale);//求出男性最高身高
-        System.out.println("Highest Female:" + highestFemale);//求出女性最高身高
+//        SparkConf sparkConf = new SparkConf().setAppName("PeopleInfoCalculator").setMaster("local[2]");
+//
+//        JavaSparkContext sc = new JavaSparkContext(sparkConf);
+//        JavaRDD<String> dataFile = sc.textFile("F:\\PeopleInfo.txt");
+//
+//
+//        JavaRDD<String> maleFilterData = dataFile.filter(s -> s.contains("M"));
+//        JavaRDD<String> femaleFilterData = dataFile.filter(s -> s.contains("F"));
+//        JavaRDD<String> maleHeightData = maleFilterData.flatMap(s -> Arrays.asList(s.split(" ")[2]).iterator());
+//        JavaRDD<String> femaleHeightData = femaleFilterData.flatMap(s -> Arrays.asList(s.split(" ")[2]).iterator());
+//        JavaRDD<Integer> maleHeightDataInt = maleHeightData.map(s -> Integer.parseInt(String.valueOf(s)));
+//        JavaRDD<Integer> femaleHeightDataInt = femaleHeightData.map(s -> Integer.parseInt(String.valueOf(s)));
+//        //sortBy(<T>,ascending,numPartitions) 解释:
+//        //第一个参数是一个函数，该函数的也有一个带T泛型的参数，返回类型和RDD中元素的类型是一致的；
+//        //第二个参数是ascending，这参数决定排序后RDD中的元素是升序还是降序，默认是true，也就是升序；
+//        //第三个参数是numPartitions，该参数决定排序后的RDD的分区个数，默认排序后的分区个数和排序之前的个数相等，即为this.partitions.size。
+//        JavaRDD<Integer> maleHeightLowSort = maleHeightDataInt.sortBy(s -> s,true,3);
+//        JavaRDD<Integer> femaleHeightLowSort = femaleHeightDataInt.sortBy(s -> s,true,3);
+//        JavaRDD<Integer> maleHeightHightSort = maleHeightDataInt.sortBy(s -> s,false,3);
+//        JavaRDD<Integer> femaleHeightHightSort = femaleHeightDataInt.sortBy(s -> s,false,3);
+//        Integer lowestMale = maleHeightLowSort.first(); //求出升序的第一个数，即最小值
+//        Integer lowestFemale = femaleHeightLowSort.first();//求出升序的第一个数，即最小值
+//        Integer highestMale = maleHeightHightSort.first();//求出降序的第一个数，即最大值
+//        Integer highestFemale = femaleHeightHightSort.first();//求出降序的第一个数，即最大值
+//
+//        System.out.println("Number of Female Peole:" + femaleHeightData.count());//求出女性的总个数
+//        System.out.println("Number of Male Peole:" + maleHeightData.count());//求出男性的总个数
+//        System.out.println("Lowest Male:" + lowestMale);//求出男性最矮身高
+//        System.out.println("Lowest Female:" + lowestFemale);//求出女性最矮身高
+//        System.out.println("Highest Male:" + highestMale);//求出男性最高身高
+//        System.out.println("Highest Female:" + highestFemale);//求出女性最高身高
 
 
 
