4dd60cd20e564699e1c9023116b0bacdd9de078d
chenggong zhou <26921832@qq.com>
2019-10-16 13:52:17 +0800

    jdk Proxy

--- util-jdk/src/main/java/com/common/jdk/proxy/ProxyTest.java
+++ util-jdk/src/main/java/com/common/jdk/proxy/ProxyTest.java
@@ -1,19 +1,19 @@
 package com.common.jdk.proxy;
 
-import java.lang.reflect.Proxy;
+import java.lang.reflect.*;
 
 /**
  * Created by zhoucg on 2019-03-17.
  */
 public class ProxyTest {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
         //设置为true,会在工程根目录生成$Proxy0.class代理类（com.sun.proxy.$Proxy0.class）
-        System.getProperties().put(
-                "sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
+        //System.getProperties().put(
+        //        "sun.misc.ProxyGenerator.saveGeneratedFiles", "true");
 
-        String saveGeneratedFiles = System.getProperty("sun.misc.ProxyGenerator.saveGeneratedFiles");
-        System.out.println(saveGeneratedFiles);
+        //String saveGeneratedFiles = System.getProperty("sun.misc.ProxyGenerator.saveGeneratedFiles");
+        //System.out.println(saveGeneratedFiles);
 
         HelloTest helloWord = new HelloTestImpl();
         CustomInvocationHandler customInvocationHandler = new CustomInvocationHandler(
@@ -24,5 +24,21 @@ public class ProxyTest {
                 helloWord.getClass().getInterfaces(), customInvocationHandler);
         //调用say方法
         proxy.say("test");
+
+
+        Class proxyClass = Proxy.getProxyClass(HelloTest.class.getClassLoader(), HelloTest.class);
+        Constructor constructor = proxyClass.getConstructor(InvocationHandler.class);
+        Object o = constructor.newInstance(customInvocationHandler);
+        HelloTest helloTest = (HelloTest) Proxy.newProxyInstance(HelloTest.class.getClassLoader(),
+                HelloTest.class.getInterfaces(), new InvocationHandler() {
+                    @Override
+                    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                        System.out.println(method.getName());
+                        return null;
+                    }
+                });
+
+        helloTest.say("你好");
+
     }
 }
