2c17a0f8f46131192d70b7d77b8d25ce5f5b4163
zhoucg <26921832@qq.com>
2020-09-16 17:28:05 +0800

    :sparkles: spark本地调试

--- util-spark/src/main/java/com/spark/demo/WordCountLocal.java
+++ util-spark/src/main/java/com/spark/demo/WordCountLocal.java
@@ -6,54 +6,38 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
 
-import java.text.SimpleDateFormat;
 import java.util.Arrays;
-import java.util.Date;
 import java.util.List;
 
 /**
+ * 本地 Spark 任务调度
+ * 1, 通过Project Structure -> modules -> Dependencies -> add jars 将spark 中的jars 加入到本地 ，开始本地调试模式
  *
- * Spark计算 jar包的结果
+ * 2, 会开启一个4040端口
  * @author zhoucg
- * @date 2020-09-13 13:54
+ * @date 2020-09-16 17:18
  */
-public class WordCount {
+public class WordCountLocal {
 
 
     public static void main(String[] args) {
 
-        String hdfsHost = args[0];
-        String hdfsPort = args[1];
-        String textFileName = args[2];
+        // 开启本地spark 调度
+        SparkConf sparkConf = new SparkConf().setAppName("WS").setMaster("local[*]");
 
-        SparkConf sparkConf = new SparkConf().setAppName("Spark WordCount Application (java)");
-
+        // 获取对应的SparkContext信息
         JavaSparkContext javaSparkContext = new JavaSparkContext(sparkConf);
 
+        // 获取对应的RDD 信息
+        JavaRDD<String> rdd = javaSparkContext.textFile("G:\\04.txt");
 
-        String hdfsBasePath = "hdfs://" + hdfsHost + ":" + hdfsPort;
-        //文本文件的hdfs路径
-        String inputPath = hdfsBasePath + "/input/" + textFileName;
-
-        // 输出文件路径
-        String outputPath = hdfsBasePath + "/output/"
-                + new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-
-        System.out.println("input path : " + inputPath);
-
-        System.out.println("output path : " + outputPath);
-
-        //导入文件
-        JavaRDD<String> textFile = javaSparkContext.textFile(inputPath);
-
-        JavaPairRDD<String, Integer> counts = textFile
+        JavaPairRDD<String, Integer> counts = rdd
                 //每一行都分割成单词，返回后组成一个大集合
                 .flatMap(s -> Arrays.asList(s.split(" ")).iterator())
                 //key是单词，value是1
                 .mapToPair(word -> new Tuple2<>(word, 1))
                 //基于key进行reduce，逻辑是将value累加
-                .reduceByKey((a, b) -> a + b);
-
+                .reduceByKey(Integer::sum);
 
         //先将key和value倒过来，再按照key排序
         JavaPairRDD<Integer, String> sorts = counts
@@ -64,16 +48,12 @@ public class WordCount {
 
         //取前10个
         List<Tuple2<Integer, String>> top10 = sorts.take(10);
-
         //打印出来
         for(Tuple2<Integer, String> tuple2 : top10){
-            System.out.println(tuple2._2() + "\t" + tuple2._1());
+            System.out.println(tuple2._2() + "  " + tuple2._1());
         }
 
-        //分区合并成一个，再导出为一个txt保存在hdfs
-        javaSparkContext.parallelize(top10).coalesce(1).saveAsTextFile(outputPath);
-
-        //关闭context
+        // 关闭
         javaSparkContext.close();
     }
 }
