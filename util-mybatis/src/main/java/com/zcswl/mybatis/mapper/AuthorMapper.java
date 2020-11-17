04709a3daa0c460ba0f6e1ee3e0840015b346056
zhoucg <26921832@qq.com>
2020-11-17 14:13:01 +0800

    :sparkles: Mapper,PageHelper 学习

--- util-mybatis/src/main/java/com/zcswl/mybatis/mapper/AuthorMapper.java
+++ util-mybatis/src/main/java/com/zcswl/mybatis/mapper/AuthorMapper.java
@@ -1,13 +1,11 @@
 package com.zcswl.mybatis.mapper;
 
 import com.zcswl.mybatis.entity.Author;
-import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author zhoucg
  * @date 2020-11-15 15:21
  */
-@Mapper
 public interface AuthorMapper {
 
 
