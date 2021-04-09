2ed690a4ef3eda212619dcd0d81a5f7d0e829f9a
zhoucg <26921832@qq.com>
2021-04-09 14:34:46 +0800

    leecode 下一个排列算法

---
+++ util-leecode/src/main/java/com/zcswl/leecode/TreeTraversal.java
@@ -0,0 +1,9 @@
+package com.zcswl.leecode;
+
+/**
+ * 树的遍历策略
+ * @author zhoucg
+ * @date 2021-04-09 9:34
+ */
+public class TreeTraversal {
+}
--- util-leecode/src/main/java/com/zcswl/leecode/node/LinkedNode.java
+++ util-leecode/src/main/java/com/zcswl/leecode/node/LinkedNode.java
@@ -3,6 +3,8 @@ package com.zcswl.leecode.node;
 /**
  * 单向 链表
  *
+ * 底层的架构核心就是数组 ，链表
+ *
  * @author zhoucg
  * @date 2021-04-07 9:40
  */
