fda4550f505025079a1854d2444af988fd1f533b
zhoucg <26921832@qq.com>
2021-04-14 11:14:03 +0800

    leecode 两数之和， 股票买卖，最大公共父节点

--- util-leecode/src/main/java/com/zcswl/leecode/FindKthLargest.java
+++ util-leecode/src/main/java/com/zcswl/leecode/FindKthLargest.java
@@ -3,6 +3,8 @@ package com.zcswl.leecode;
 /**
  * 在未排序的数组中找到第 k 个最大的元素。请注意，你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
  *
+ * https://leetcode-cn.com/problems/kth-largest-element-in-an-array/
+ *
  * 输入: [3,2,1,5,6,4] 和 k = 2
  * 1 2 3 4 5 6
  * 输出: 5
@@ -14,8 +16,55 @@ package com.zcswl.leecode;
  */
 public class FindKthLargest {
 
-
+    /**
+     * 排序算法，
+     */
     public int findKthLargest(int[] nums, int k) {
         return 1;
+    }
+
+
+    /**
+     * 冒泡排序
+     * 插入排序
+     * 选择排序
+     */
+    public void sort(int[] nums) {
+        // 插入排序
+        for (int i = 1; i < nums.length ; i++) {
+            for (int j = i; j > 0;j--) {
+                if (nums[j] < nums[j-1]) {
+                    int tmp = nums[j-1];
+                    nums[j-1] = nums[j];
+                    nums[j] = tmp;
+                } else{
+                    break;
+                }
+            }
+        }
+
+        // 冒泡排序
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length - i - 1;j++) {
+                if (nums[j] > nums[j+1]) {
+                    int tmp = nums[j+1];
+                    nums[j+1] = nums[j];
+                    nums[j] = tmp;
+                }
+            }
+        }
+        // 选择排序，以指定的字段为基准
+        for (int i = 0; i< nums.length ;i++) {
+            int sortIndex = i;
+            for (int j = sortIndex+1; j< nums.length ; j++) {
+                if (nums[i] > nums[j]) {
+                    sortIndex = j;
+                }
+                //交换对应的 sortIndex 和i的值
+                int tmp = nums[i];
+                nums[i] = nums[sortIndex];
+                nums[sortIndex] = tmp;
+            }
+        }
     }
 }
--- util-leecode/src/main/java/com/zcswl/leecode/MaxProfitDemo.java
+++ util-leecode/src/main/java/com/zcswl/leecode/MaxProfitDemo.java
@@ -9,6 +9,12 @@ package com.zcswl.leecode;
 public class MaxProfitDemo {
 
 
+    public static void main(String[] args) {
+        int[] arr = {7,1,5,3,6,4};
+
+    }
+
+
     public int maxProfit(int[] arr) {
         int minPrice = Integer.MAX_VALUE;
         int maxProfit = 0;
@@ -20,5 +26,25 @@ public class MaxProfitDemo {
             }
         }
         return maxProfit;
+    }
+
+    /**
+     * 股票计算的第二种方法，实际上就是计算差值
+     * 7,1,5,3,6,4
+     */
+    public int maxProfit1(int[] arr) {
+        if (arr.length == 0) { // 4 - 2 + 3
+            return 0;
+        }
+        int ans = 0;
+        int sum = 0;
+        for (int i = 1; i < arr.length ; i++) {
+            sum += arr[i] - arr[i-1];
+            ans = Math.max(ans, sum); // 4
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return ans;
     }
 }
