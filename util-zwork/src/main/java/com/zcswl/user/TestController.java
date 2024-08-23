5cfed1a3b1c2a18493a679f48513bc920e10f499
zhoucg <26921832@qq.com>
2024-08-23 23:04:19 +0800

    with mock/ just return simple flag(rebase)

--- util-zwork/src/main/java/com/zcswl/user/TestController.java
+++ util-zwork/src/main/java/com/zcswl/user/TestController.java
@@ -17,7 +17,9 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
+        // mock
         List<SpringApplicationRunListener> springApplicationRunListeners = SpringFactoriesLoader.loadFactories(SpringApplicationRunListener.class, null);
         return "1";
+        // just return simple 1
     }
 }
