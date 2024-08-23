37e4ecd8ae4ca2c752351e56a3d4f934e531149d
zhoucg <26921832@qq.com>
2024-08-23 23:04:19 +0800

    with mock

--- util-zwork/src/main/java/com/zcswl/user/TestController.java
+++ util-zwork/src/main/java/com/zcswl/user/TestController.java
@@ -17,6 +17,7 @@ public class TestController {
 
     @GetMapping("/test")
     public String test() {
+        // mock
         List<SpringApplicationRunListener> springApplicationRunListeners = SpringFactoriesLoader.loadFactories(SpringApplicationRunListener.class, null);
         return "1";
     }
